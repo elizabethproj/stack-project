67ab9f0c3c70f35dbec4470305a2b435ea297c79
beth-codes <70582711+beth-codes@users.noreply.github.com>
2023-06-22 18:24:39 +0100

    Update Project4.js

--- src/componenets/Project4.js
+++ src/componenets/Project4.js
@@ -10,7 +10,7 @@ function Project4() {
          The Rose Hotel Project is a responsive landing page built with PHP, CSS, SASS, WordPress and MySQL. As a WordPress Developer, I am responsible for creating a customized design that reflects the hotel's unique aesthetic, as well as configuring & managing all necessary plugins required to ensure the project is optimized for the best user experience. Additionally, I am responsible for integrating all of the back-end data into the WordPress database so that necessary feature & content updates are reflected within the project.
         </p>
         <h3 className="project-link">
-          <a href="http://rosehotel.x10.mx target="_blank" rel="noreferrer">
+          <a href="http://rosehotel.x10.mx" target="_blank" rel="noreferrer">
             <span>View Site</span>
           </a>
           <br />
