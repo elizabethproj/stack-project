c3f358e9e71c3804455b0671af813d5224f9b406
beth-codes <70582711+beth-codes@users.noreply.github.com>
2023-12-14 19:21:35 +0000

    Update Project2.js

--- src/componenets/Project2.js
+++ src/componenets/Project2.js
@@ -4,15 +4,15 @@ function Project2() {
   return (
     <div className="project1-cont active">
       <div className="project-header">
-        <h2>Beth Plugins</h2>
+        <h2>Ray's Portfolio</h2>
       </div>
       <div className="about-text">
         <p>
-          Beth Plugins is a custom WordPress theme created mainly for showcasing custom plugins, currently has two functional plugins, a customized Contact Form and a Tag Category for better organization of contents. This project was developed using a combination of PHP, CSS, SASS, JavaScript and jQuery. 
+         Ray's project was implemented using HTML, CSS/SASS, JavaScript, React, and PHP ensuring a seamless and responsive user experience across different devices. The website includes various custom blocks and interactive elements, enhancing the overall aesthetic appeal and engagement for users.
         </p>
         <h3 className="project-link">
           <a
-            href="https://orangelineinteriors.com"
+            href="https://raylutzky.com/"
             target="_blank"
             rel="noreferrer"
           >
