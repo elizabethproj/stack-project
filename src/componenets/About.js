4ecacf8625b3d6c2d8aa32dd69288a2ce186bc40
beth-codes <70582711+beth-codes@users.noreply.github.com>
2024-03-09 13:27:56 +0000

    Update About.js

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -18,7 +18,7 @@ function About({ open }) {
             </div>
             <div className={`about-text-cont`}>
               <p className="about-text">
-                Hello! I'm Elizabeth, a Web developer with over four years of experience. My expertise lies in creating modern, user-friendly, and dynamic web applications. I have good experience with various web technologies such as HTML, CSS, JavaScript, React, and PHP, among others. Additionally, I have a good understanding of UX and UI design principles.
+                Hello! I'm Elizabeth, a Software Engineer with over five years of experience. My expertise lies in creating modern, user-friendly, and dynamic web applications. I have experience working with various web technologies such as HTML, CSS, JavaScript, React, and PHP, among others. Additionally, I have a good understanding of UX and UI design principles.
                   <br/>
                 In my portfolio, you can find some of my recent projects that highlight my ability to create visually appealing, responsive, and SEO-optimized websites and web applications. If you'd like to discuss my portfolio in more detail, please feel free to reach out to me. Thank you!
               </p>
